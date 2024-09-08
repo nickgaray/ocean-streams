46c40c80948055c78dcb915e08e8f9b00122f9da
Nick Garay <nicolasgaray@icloud.com>
2024-09-08 15:15:55 -0400

    Simplify launch scripts for baseline zip deployment

---
+++ dist/scripts/launch.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xmx2g -Dlogback.configurationFile=./config/logback.xml -cp "lib/*" -Djava.system.class.loader="org.sensorhub.utils.NativeClassLoader" org.sensorhub.impl.SensorHub ./config/config.json ./db
