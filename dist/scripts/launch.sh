0321081cd86f2c30d998a522ff6ef5e7bad008fd
Nick Garay <nicolasgaray@icloud.com>
2024-12-18 21:27:43 -0500

    Fix keystore and truststore related setup

--- dist/scripts/launch.sh
+++ dist/scripts/launch.sh
@@ -8,9 +8,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 java -Xmx2g \
 -Dlogback.configurationFile=./config/logback.xml \
 -cp "lib/*" \
--Djavax.net.ssl.keyStore="./config/osh-keystore.p12" \
--Djavax.net.ssl.keyStorePassword="osh-keystore" \
--Djavax.net.ssl.trustStore="$SCRIPT_DIR/trustStore.jks" \
--Djavax.net.ssl.trustStorePassword="changeit" \
 -Djava.system.class.loader="org.sensorhub.utils.NativeClassLoader" \
+-Djavax.net.ssl.keyStore="./config/keystore.p12" \
+-Djavax.net.ssl.keyStorePassword="keystore" \
+-Djavax.net.ssl.trustStore="$SCRIPT_DIR/config/trustStore.jks" \
+-Djavax.net.ssl.trustStorePassword="changeit" \
 org.sensorhub.impl.SensorHub ./config/config.json ./db
